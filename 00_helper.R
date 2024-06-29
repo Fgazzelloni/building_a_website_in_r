5933e05df99b243ae017a48a331c3c4496016733
fgazzelloni <docksbox@pm.me>
2024-06-29 17:57:38 +0200

    updating with instructions

--- 00_helper.R
+++ 00_helper.R
@@ -1,14 +1,16 @@
-# INSTRUCTION TO SETUP GITHUB REPOSITORY
+##### INSTRUCTION #### 
+
+##### SETUP GITHUB REPOSITORY #####
 
 # When you created this project in RStudio, you should have checked the box "create a git repository".
 
 # Activate the repository to be able to relate with your github account.
 
-# Step 1:
+# Step 1 ####
 
 # If you have created a github account, then create a new repository with the same name of the project's name in RStudio, do not initialize with README.md file. It will present a page with instructions to push an existing repository from the command line. Before to use that code follow the next steps.
 
-# Step 2:
+# Step 2 ####
 
 # Authenticate your github account with RStudio.
 # To do this, generate a secure key with the following code:
@@ -24,17 +26,36 @@ usethis::edit_r_environ()
 
 # Paste the key there as: GITHUB_PAT='YOURKEY'
 
+# Step 3 ####
+
 # Then go back to github and copy and paste the lines of codes 
 # paste them all together in Rstudio terminal:
 
-##### These are the lines of code you should see on your project's github page #####
+## These are the lines of code you should see on your project's github page ##
 
-# echo "# building_a_website_in_r" >> README# .md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin git@github.com# :YOUR GITHUB USERNAME/building_a_website_in_r.git
-# git push -u origin main
+echo "# building_a_website_in_r" >> README# .md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com# :YOUR GITHUB USERNAME/building_a_website_in_r.git
+git push -u origin main
 
-##### End #####
+## End ##
+
+#### QUARTO CLI ####
+
+# In terminal type:
+
+quarto render # to compile all the files in the project
+quarto preview # to see the website in the browser
+quarto publish # to publish the website
+
+#### QUARTOPUB FOR PUBLISHING ####
+
+# To select the option to publish to quartopub you need a quartopub account.
+
+
+
+
+
